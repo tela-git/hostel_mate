c2265d23303f6b4c43413f1bb205c8b35e685784
Umesh <umesh.t.5005@gmail.com>
2025-02-16 19:34:19 +0530

    Add ExploreViewModel to gitignore

--- app/src/main/java/com/example/hostelmate/hostel/data/di/AppModule.kt
+++ app/src/main/java/com/example/hostelmate/hostel/data/di/AppModule.kt
@@ -1,10 +1,9 @@
 package com.example.hostelmate.hostel.data.di
 
-import androidx.lifecycle.viewmodel.compose.viewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 import com.example.hostelmate.hostel.presentation.ui.screens.authentication.AuthViewModel
-import com.example.hostelmate.hostel.presentation.ui.screens.main.explore.ExploreViewModel
+import com.example.hostelmate.hostel.presentation.ui.screens.main.explore.viewmodel.ExploreViewModel
 
 val appModule = module {
     viewModelOf(::AuthViewModel)
